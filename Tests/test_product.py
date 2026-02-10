57466c556e9dfe4197e20180ba57876cb74f8862
ashishkhobragade2001 <ashish999khobragade@gmail.com>
2026-02-10 10:12:42 +0530

    file update

--- Tests/test_product.py
+++ Tests/test_product.py
@@ -1,16 +1,14 @@
-from Locators.products_locators import ProductLocators
 from Pages.products_page import ProductPage
-from Utilities.base_page import BasePage
 
 
 class TestProducts:
 
     def test_products_add_to_carts(self, driver, logger):
         pp = ProductPage(driver, logger)
-        pp.login_to_webpage(username="ashish@outlook.com", password="ashish123")
+        pp.login_to_webpage(username="ashish@outlook.com", password="00ashish123")
         pp.select_products()
         pp.card_details_page(
-            name_on_card="Sagar Seth",
+            name_on_card="Neha Seth",
             card_number="111144443333",
             cvv_number="123",
             expiry_month="12",
