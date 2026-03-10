dd3d0cc68de0dcbf2709f4592d9254fd64df200a
ashishkhobragade2001 <ashish999khobragade@gmail.com>
2026-03-10 20:01:53 +0530

    remove unwanted comments

--- Tests/test_login.py
+++ Tests/test_login.py
@@ -2,15 +2,6 @@ import pytest
 from Pages.login_page import LoginPage
 from Utilities.excel_reader import get_login_data
 
-"""@pytest.mark.parametrize("EMAIL_INPUT,password", [
-    ("test1@mail.com", "12345"),
-    ("test2@mail.com", "wrongpass")
-])
-def test_login(driver, EMAIL_INPUT, password):
-    lp = LoginPage(driver)
-    lp.open_login()
-    lp.login(EMAIL_INPUT, password)"""
-
 
 @pytest.mark.login
 @pytest.mark.parametrize("username, password, expected_result", get_login_data())
@@ -20,5 +11,3 @@ def test_login(driver, logger, username, password, expected_result):
 
     actual_result = "success" if lp.is_login_successful() else "failure"
     assert actual_result == expected_result, f"Expected: {expected_result} but we got: {actual_result}"
-
-# update file for test scm polling
