bf3edaffd5a1cdfd015ecf72486659e1ac47894a
ashishkhobragade2001 <ashish999khobragade@gmail.com>
2026-03-10 20:06:26 +0530

    update

--- Tests/test_login.py
+++ Tests/test_login.py
@@ -11,3 +11,7 @@ def test_login(driver, logger, username, password, expected_result):
 
     actual_result = "success" if lp.is_login_successful() else "failure"
     assert actual_result == expected_result, f"Expected: {expected_result} but we got: {actual_result}"
+
+
+# file update
+# test for pole scm
