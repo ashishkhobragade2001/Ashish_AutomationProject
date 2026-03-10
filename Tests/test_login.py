ab8c80a516ecb1e49abf487cc0d268dcc4872366
ashishkhobragade2001 <ashish999khobragade@gmail.com>
2026-03-10 19:46:28 +0530

    test scm poll

--- Tests/test_login.py
+++ Tests/test_login.py
@@ -20,3 +20,5 @@ def test_login(driver, logger, username, password, expected_result):
 
     actual_result = "success" if lp.is_login_successful() else "failure"
     assert actual_result == expected_result, f"Expected: {expected_result} but we got: {actual_result}"
+
+# update file for test scm polling
